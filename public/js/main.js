2c85b347f401372407075805a19fa553d6ff2453
zmedailleu <zacharymedailleu@gmail.com>
2025-02-14 19:17:45 -0500

    Created working exercise page which displays a number of polygons

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+
+function changePolygonSize() {
+    var polygon = document.getElementById("sizePolygon");
+    var currentRadius = polygon.getAttribute("r");
+    var newRadius = parseInt(currentRadius) + 3;
+    polygon.setAttribute("r", newRadius.toString());
+}
